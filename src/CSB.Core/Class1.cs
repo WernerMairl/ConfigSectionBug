ad0b21814fb5c6f29b69dfe4842e2c2722bd9d4d
Werner Mairl <werner.mairl@gmail.com>
2017-07-19 15:31:51 +0200

    fix references

--- src/CSB.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSB.Core
-{
-    public class Class1
-    {
-    }
-}
--- src/CSB.Test/UnitTest1.cs
+++ src/CSB.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using Xunit;
-
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration.Json;
+using System.IO;
 namespace CSB.Test
 {
     public class UnitTest1
@@ -8,6 +10,14 @@ namespace CSB.Test
         [Fact]
         public void Test1()
         {
+
+            //IConfigurationBuilder builder = new ConfigurationBuilder()
+            //    .SetBasePath("")
+            //    .AddJsonFile("appsettings.json", optional: false);
+
+            //IConfiguration config =  builder.Build();
+
+
 
         }
     }
